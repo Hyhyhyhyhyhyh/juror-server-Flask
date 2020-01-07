031c95d3e9285a46576603bc45a0e395bc7e3c5b
hyh744 <814919935@qq.com>
2020-01-07 23:18:35 +0800

    [+]新增Nginx日志分析接口v0.1

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_cors import *
 
 from app.user_views import *
-from app.peotry_views import *
+from app.analyze_views import *
 from tests.tests import *
 
 app = Flask(__name__, template_folder=None, static_folder=None)
@@ -17,7 +17,7 @@ api.add_resource(ModifyUser,    '/api/user/modify')
 api.add_resource(UserList,      '/api/user/query/<string:token>')
 api.add_resource(DropUser,      '/api/user/drop')
 api.add_resource(Test,          '/api/test/<string:token>')
-api.add_resource(WordFreqyency, '/api/analyze/wordfrequency/<string:database>/<string:table_name>/<string:col_name>')
+api.add_resource(An, '/api/analyze/wordfrequency/<string:database>/<string:table_name>/<string:col_name>')
 # api.add_resource(CreateUser, '/api/juror/query')  
 
 if __name__ == '__main__':
